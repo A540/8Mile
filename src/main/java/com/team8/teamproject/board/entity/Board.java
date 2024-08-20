59bf66de97c644e46907bb7c6b8df79579a05437
[CLOUD4] 심우민 <worejj@naver.com>
2024-08-20 13:49:20 +0900

    Feat:Board 엔티티

---
+++ src/main/java/com/team8/teamproject/board/entity/Board.java
@@ -0,0 +1,53 @@
+package com.team8.teamproject.board.entity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class Board {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String description;
+    private LocalDateTime createdAt;
+    private LocalDateTime modifiedAt;
+
+//    @ManyToOne
+//    @JoinColumn(name = "user_id")
+//    private User user;
+
+//    @OneToMany(mappedBy = "board")
+//    private List<Post> posts = new ArrayList<>();
+
+    private Board(String name, String description) {
+
+        this.name = name;
+        this.description = description;
+        this.createdAt = LocalDateTime.now();
+        this.modifiedAt = LocalDateTime.now();
+    }
+
+
+//    // 연관 관계 편의 메서드
+//    public linkUser(User user) {
+//        this.user = user;
+//        user.getBoards().add(this);
+//    }
+
+    //생성 메서드
+    public static Board createBoard(String name, String description, LocalDateTime createdAt) {
+        Board board = new Board(name, description);
+        return board;
+    }
+
+
+
+}
