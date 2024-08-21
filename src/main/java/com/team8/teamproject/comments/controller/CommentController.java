60177e7dab2dcd36124e39c124329fb1d5fa519f
[CLOUD4] 박진영 <wlsdud2528@naver.com>
2024-08-21 10:35:12 +0900

    feat: 코멘트 삭제 기능 추가

--- src/main/java/com/team8/teamproject/comments/controller/CommentController.java
+++ src/main/java/com/team8/teamproject/comments/controller/CommentController.java
@@ -16,22 +16,32 @@ public class CommentController {
 
     private final CommentService commentService;
 
+    // 게시글의 id를 받아 해당 댓글만 모두 출력
     @GetMapping("/comment/{postId}")
     public ResponseEntity<List<Comments>> findComments(@PathVariable("postId") Long postId){
         List<Comments> Comments = commentService.findAll(postId);
         return new ResponseEntity<>(Comments, HttpStatus.OK);
     }
 
+    // 댓글 생성
     @PostMapping("/comment/{postId}")
     public ResponseEntity<Comments> saveComments(@PathVariable("postId") Long id, @RequestBody AddCommentRequest addDTO){
         Comments savedComment = commentService.save(id, addDTO);
         return new ResponseEntity<>(savedComment, HttpStatus.CREATED);
     }
 
+    // 댓글 수정
     @PostMapping("/comment/{commentId}/edit")
     public ResponseEntity<Comments> updateComments(@PathVariable("commentId") Long id, @RequestBody AddCommentRequest updateDTO){
         Comments updatedComment = commentService.update(id, updateDTO);
         return new ResponseEntity<>(updatedComment, HttpStatus.OK);
+    }
+
+    // 댓글 삭제
+    @DeleteMapping("/comment/{commentId}")
+    public ResponseEntity<Void> deleteComment(@PathVariable("commentId") Long id){
+        commentService.delete(id);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
 }
--- src/main/java/com/team8/teamproject/comments/dto/AddCommentRequest.java
+++ src/main/java/com/team8/teamproject/comments/dto/AddCommentRequest.java
@@ -1,11 +1,10 @@
 package com.team8.teamproject.comments.dto;
 
-import com.team8.teamproject.comments.domain.Comments;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-
+// 저장과 수정시에 요청으로 받을 DTO
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
