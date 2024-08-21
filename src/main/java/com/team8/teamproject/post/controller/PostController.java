d6cb449a3cf10ec0293fddce69a68068b6811d0f
[CLOUD4] 홍민영 <47109@naver.com>
2024-08-21 16:58:44 +0900

    홍민영#4 Update 구현

--- src/main/java/com/team8/teamproject/post/controller/PostController.java
+++ src/main/java/com/team8/teamproject/post/controller/PostController.java
@@ -2,37 +2,53 @@ package com.team8.teamproject.post.controller;
 
 import com.team8.teamproject.post.domain.Post;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 import com.team8.teamproject.post.service.PostService;
-import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class PostController {
 
-    private final PostService service;
+    private final PostService postService;
 
-    public PostController(PostService service){
-        this.service = service;
+    public PostController(PostService postService){
+        this.postService = postService;
     }
 
-    @GetMapping("/posts")
-    public String getPost() {
+    // Create
+    @GetMapping("/posts/create")
+    public String getCreatePost(){
+        return "post/createPost";
+    }
+    @PostMapping("/posts/create")
+    public void createPost(Post post) {
+        postService.createPost(post);
+    }
+
+    // Read boardId, CommentId 연관 관계 설정 후 진행
+    @GetMapping("/posts/{postId}")
+    public String readPost(@PathVariable Long postId, Model model) {
+        Post readPost = postService.readPost(postId);
+        model.addAttribute("post", readPost);
         return "post/post";
     }
 
-    @GetMapping("/posts/create")
-    public String getCreatePost() {
-        return "post/createPost";
-    }
-
-    @GetMapping("/posts/edit")
-    public String getEditPost() {
+    // Update
+    @GetMapping("/posts/{postId}/edit")
+    public String getEditPost(@PathVariable Long postId, Model model){
+        Post readPost = postService.readPost(postId);
+        model.addAttribute("post", readPost);
         return "post/editPost";
     }
-
-    //Create
-    @PostMapping("/posts/create")
-    public void createPost(Post post){
-        service.createPost(post);
+    @PostMapping("/posts/{postId}/edit")
+    public void editPost(@PathVariable Long postId, Post post) {
+        postService.editPost(postId, post);
     }
+
+    // Delete Read 완성 후 진행
+    /*
+    @GetMapping("/posts/{postId}/delete")
+    public String getDeletePost(@PathVariable Long postId) {
+    }
+    */
 }
