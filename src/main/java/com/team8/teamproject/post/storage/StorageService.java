d9b1cbb38a29299d3f01edf7a3d5ade30926e6fd
[CLOUD4] 홍민영 <47109@naver.com>
2024-08-27 18:31:34 +0900

    홍민영#12 파일 업로드 추상화, 멤버 연관관계 설정

---
+++ src/main/java/com/team8/teamproject/post/storage/StorageService.java
@@ -0,0 +1,14 @@
+package com.team8.teamproject.post.storage;
+
+import com.team8.teamproject.login.entity.Member;
+import com.team8.teamproject.post.dto.FileDTO;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+public interface StorageService {
+    void createLocalPost(Long boardId, String title, String content, MultipartFile file, Member member) throws IOException;
+    Long saveFile(FileDTO fileDTO);
+    FileDTO getFile(Long id);
+    byte[] getImage(Long fileId) throws IOException;
+}
