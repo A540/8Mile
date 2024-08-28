e70e111bec47241379c1b9beecc5c05e7a0703d7
[CLOUD4] 심우민 <worejj@naver.com>
2024-08-28 16:21:47 +0900

    Feat: 북마크 기능 프레젠테이션 계층 개발

--- src/main/java/com/team8/teamproject/bookmark/BookmarkController.java
+++ src/main/java/com/team8/teamproject/bookmark/BookmarkController.java
@@ -1,10 +1,12 @@
 package com.team8.teamproject.bookmark;
 
 import com.team8.teamproject.board.controller.dto.BoardsViewDto;
+import com.team8.teamproject.board.controller.dto.MemberViewDto;
 import com.team8.teamproject.board.domain.Board;
 import com.team8.teamproject.bookmark.domain.Bookmark;
 import com.team8.teamproject.bookmark.service.BookmarkService;
 import com.team8.teamproject.login.entity.Member;
+import com.team8.teamproject.login.service.MemberService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
@@ -12,10 +14,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -26,27 +27,50 @@ import java.util.stream.Collectors;
 public class BookmarkController {
 
     private final BookmarkService bookmarkService;
+    private final MemberService memberService;
 
     //== 북마크 등록, 삭제 처리 ==//
     @ResponseBody
     @PostMapping("/bookmark/{boardId}")
     public ResponseEntity<Map<String, String>> clickBookmark(@PathVariable(value = "boardId") Long boardId, HttpServletRequest request) {
 
+        //세션 정보 가져오기
         HttpSession session = request.getSession(false);
         if (session == null || session.getAttribute("loggedInUser") == null) {
             log.info("no user");
             return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
         }
-
         Member loggedInUser = (Member) session.getAttribute("loggedInUser");
+
         Map<String, String> data = bookmarkService.clickBookmark(loggedInUser.getId(), boardId);
 
         return new ResponseEntity<>(data, HttpStatus.OK);
     }
 
+    /**
+     * 북마크 삭제
+     * 북마크 페이지에서는 삭제 알림을 띄어준 후 삭제를 진행
+     * 게시판 페이지에서의 북마크 클릭과 달리 동작
+     */
+    @DeleteMapping("/bookmark/{boardId}")
+    public ResponseEntity<Void> deleteBookmark(@PathVariable(value = "boardId") Long boardId, HttpServletRequest request) {
+
+        //세션 정보 가져오기
+        HttpSession session = request.getSession(false);
+        if (session == null || session.getAttribute("loggedInUser") == null) {
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+        }
+        Member loggedInUser = (Member) session.getAttribute("loggedInUser");
+
+        bookmarkService.deleteBookmark(loggedInUser.getId(), boardId);
+
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+
     //== 북마크 페이지 VIEW ==//
     @GetMapping("/bookmark/{memberId}")
-    public ResponseEntity<List<BoardsViewDto>> getBookMarkPage(@PathVariable(value = "memberId") Long memberId, HttpServletRequest request) {
+    public String getBookMarkPage(@PathVariable(value = "memberId") Long memberId, Model model) {
 
         List<Bookmark> bookmarkList = bookmarkService.findAllByMember(memberId);
 
@@ -69,7 +93,14 @@ public class BookmarkController {
                 })
                 .collect(Collectors.toList());
 
+        model.addAttribute("boards", boardsViewDtos);
 
-        return new ResponseEntity<>(boardsViewDtos, HttpStatus.OK);
+
+        //회원 정보 조회
+        Member member = memberService.findById(memberId);
+        model.addAttribute("member", new MemberViewDto(member));
+
+
+        return "bookmark/bookmark";
     }
 }
